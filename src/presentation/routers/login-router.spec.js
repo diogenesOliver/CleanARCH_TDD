47ed51a2f6d8ae3bc096dd19b21c9f715a9900fb
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-02-02 01:00:38 -0300

    feat: Creting first test from clas login User

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -9,11 +9,23 @@ class LoginRouter {
 }
 
 describe('Login router', () => {
-  test('Should return 400 if no meial is provide', () => {
+  test('Should return 400 if no email is provide', () => {
     const sut = new LoginRouter()
     const httRequest = {
       body: {
         password: 'anyPassword'
+      }
+    }
+
+    const httpResponse = sut.route(httRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provide', () => {
+    const sut = new LoginRouter()
+    const httRequest = {
+      body: {
+        email: 'anyEmail@.com'
       }
     }
 
